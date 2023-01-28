d438554b7561bf921b237a557437188fff693f3e
Shogo Kitada <contact@shogo0x2e.com>
2023-01-29 01:40:14 +0900

    装飾周りを追加して build まで

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 export default function Custom404() {
     return (
         <main className="main">
-            <p>There's no page. Please try again.</p>
+            <p>There&apos;s no page. Please try again.</p>
         </main>
     )
 }
